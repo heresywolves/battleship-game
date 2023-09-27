72008297b696b4ada7c82679f7551ca57759c65b
Howl Kimura <howlkimuradev@gmail.com>
2023-09-27 11:00:53 +0300

    Refactor ship tests with beforeEach block

--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -1,34 +1,34 @@
 import Ship from '../src/Ship';
 
+let newShip;
+
+beforeEach(() => {
+  newShip = Ship(3);
+});
+
 test('ship creation on correct length', () => {
-  const newShip = Ship(5);
-  expect(newShip.length).toBe(5);
+  expect(newShip.length).toBe(3);
 });
 
 test('hit ship 1 time', () => {
-  const newShip = Ship(5);
   newShip.hit();
-  console.log(newShip.hit);
   expect(newShip.hits).toBe(1);
 });
 
-test('hit ship 3 times', () => {
-  const newShip = Ship(5);
+test('hit ship 2 times', () => {
   newShip.hit();
   newShip.hit();
-  newShip.hit();
-  expect(newShip.hits).toBe(3);
+  expect(newShip.hits).toBe(2);
 });
 
 test('sink ship', () => {
-  const newShip = Ship(2);
+  newShip.hit();
   newShip.hit();
   newShip.hit();
   expect(newShip.sunk).toBeTruthy();
 });
 
 test('ship not sunk if not enought hits', () => {
-  const newShip = Ship(3);
   newShip.hit();
   newShip.hit();
   expect(newShip.sunk).toBeFalsy();
