fe0f1b0fe38e9fb232ebcc4563c4fea76b0fb1b4
Howl Kimura <howlkimuradev@gmail.com>
2023-09-27 21:31:30 +0300

    Add functions to GameBoard to search for sqaures and availability

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,6 +1,10 @@
+import Ship from './Ship';
+
 const GameBoard = () => {
   const boardSize = 10;
   const board = [];
+  // numbers in array represent the sizes of ships
+  const shipsToPlace = [5, 4, 4, 3, 3, 3, 2, 2, 2, 2];
 
   function initBoard() {
     for (let x = 1; x <= boardSize; x++) {
@@ -16,7 +20,44 @@ const GameBoard = () => {
   }
   initBoard();
 
-  return { board };
+  function getSquare(x, y) {
+    board.forEach((square) => {
+      if (square.x === x && square.y === y) {
+        return square;
+      }
+    });
+    // If the for block didn't return anything
+    throw new Error('Error: Couldn\'t find x and y in getSquare function');
+  }
+
+  function occupySquare(x, y) {
+    const square = getSquare(x, y);
+    square.hasShip = true;
+  }
+
+  function checkPlacementValidity(size, orientation, x, y) {
+    let xOffset = x;
+    let isAvailable = true;
+    if (orientation === 'horizontal') {
+      for (let i = 0; i < size; i++) {
+        xOffset += 0;
+        if (y < 1 && y > 10 && xOffset < 1 && xOffset > 10) {
+          isAvailable = false;
+        }
+      }
+    }
+    return isAvailable;
+  }
+
+  function placeShip(x, y) {
+    if (shipsToPlace.length === 0) {
+      throw new Error('Error: Out of available ships to place');
+    }
+    const size = shipsToPlace.shift();
+    const ship = Ship(size);
+  }
+
+  return { board, placeShip };
 };
 
 export default GameBoard;
--- src/Ship.js
+++ src/Ship.js
@@ -14,6 +14,7 @@ const Ship = (length) => {
     length,
     sunk,
     hit,
+    orientation: horizontal,
   };
 };
 
